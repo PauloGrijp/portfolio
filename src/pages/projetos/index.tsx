915e98663fe0ca6dab62582daaf2cddeaa2df6c7
Paulo Xavier <pxavierf@gmail.com>
2021-10-02 02:12:50 -0300

    creating projects page

--- src/pages/projetos/index.tsx
+++ src/pages/projetos/index.tsx
@@ -1,9 +1,37 @@
+import Header from '../../components/Header';
+import ProjectItem from '../../components/ProjectItem';
 import { ProjectsContainer } from '../../styles/ProjectsStyles';
 
 export default function Projetos() {
   return (
     <ProjectsContainer>
-      <h1>Projetos</h1>
+      <Header />
+      <main className="container">
+        <ProjectItem
+          title="Projeto 1"
+          type="Website"
+          imgUrl="https://img.freepik.com/free-vector/black-elegant-website-design-template-business-development_136420-21.jpg?size=626&ext=jpg"
+          slug="teste"
+        />
+        <ProjectItem
+          title="Projeto 1"
+          type="Website"
+          imgUrl="https://img.freepik.com/free-vector/black-elegant-website-design-template-business-development_136420-21.jpg?size=626&ext=jpg"
+          slug="teste"
+        />
+        <ProjectItem
+          title="Projeto 1"
+          type="Website"
+          imgUrl="https://img.freepik.com/free-vector/black-elegant-website-design-template-business-development_136420-21.jpg?size=626&ext=jpg"
+          slug="teste"
+        />
+        <ProjectItem
+          title="Projeto 1"
+          type="Website"
+          imgUrl="https://img.freepik.com/free-vector/black-elegant-website-design-template-business-development_136420-21.jpg?size=626&ext=jpg"
+          slug="teste"
+        />
+      </main>
     </ProjectsContainer>
   );
 }
